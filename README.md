190d0688560f15f4c82937ed57c74e2b867e08f2
Jesica Gutierrez <jesica89@gmail.com>
2023-01-22 20:04:26 -0300

    web cargada

---
+++ README.md
@@ -0,0 +1,4 @@
+# utilindo.github.io
+Sitio web para emprendimiento de tejido a maquina, el mismo dicta cursos, seminarios y tutoriales.
+#
+Actualmente esta en proceso de reconstrucción de diseño y estructura.
---
+++ java/eventos.js
@@ -0,0 +1,18 @@
+function botonNosotros() {
+    location.href="#nosotros"
+}
+function botonCursos() {
+    location.href="#cursos"
+}
+function botonPatrones(){
+    location.href="#patrones"
+}
+function volver(){
+    location.href="#logoimg"
+}
+function comprarCurso() {
+    window.open("https://forms.gle/fYJqoonyUsVwhHPo9")
+}
+function comprarPatron() {
+    window.open("https://forms.gle/yqPB88jDLAhsJdtU9")
+}
